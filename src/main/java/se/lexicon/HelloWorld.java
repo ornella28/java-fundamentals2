5dfe03dcf69ff5b6c4c93286f6cd5ed54a790eb0
ornella28 <angelnibaruta88@gmail.com>
2025-11-25 10:49:17 +0100

    Initial commit

---
+++ src/main/java/se/lexicon/HelloWorld.java
@@ -0,0 +1,17 @@
+package se.lexicon;
+
+public class HelloWorld {
+
+    //public static void main(String[] args) {
+
+        //System.out.println("Hello World!");
+
+
+    void main(){
+        //System.out.println("Hello World");
+        IO.println("Hello World");
+    }
+
+
+    }
+
